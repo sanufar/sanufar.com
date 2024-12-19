57120ad561357d1ca0ea28b164d25d7e6b7a9f49
Farhan Sanukri <farhan.sanukri24@gmail.com>
2024-12-18 19:19:07 -0500

    chore: fixed some issues with layout and page rendering

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,10 +5,12 @@
 </script>
 
 
-<div class="font-ibm flex min-h-screen overflow-auto flex-col sm:flex-row bg-[#304FA5] text-[#EFF2FA]">
+<div class="flex min-h-screen  flex-col sm:flex-row bg-[#304FA5] text-[#EFF2FA]">
+	<div class=" sticky top-0">
 	  <Sidebar />
+	</div>
 
-	<div class="mx-4 p-4 sm:pt-16">
+	<div class="font-extralight overflow-auto mx-4 p-4 sm:pt-16">
     <slot />
 	</div>
 </div>
